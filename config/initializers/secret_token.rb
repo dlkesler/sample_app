e07dc0910012660eba6a2864283741b4af8bcfcd
Doug Kesler <doug@motiveflow.com>
2012-09-05 09:36:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ee7279447719fca595d46168178ccce981da367cc067a7866effc7871b0d4f7cec8a7f998cd980e3df20b34623ce785fc10f1f3dcdec8f111d42d76cf23d12df'
